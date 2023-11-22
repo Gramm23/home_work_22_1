ecd0f271c1d12dd6425d239b666832c3506a0fed
sergey <serglapinofff@gmail.com>
2023-11-22 10:34:37 +0300

    init

---
+++ product/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from product.views import *
+
+app_name = 'product'
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='index'),
+    path('view/<int:pk>/', ProductDetailView.as_view(), name='view'),
+    path('contacts/', contacts, name='contacts'),
+    path('create/', ProductCreateView.as_view(), name='create'),
+    path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete')
+]
